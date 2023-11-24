60332b0c503b9e7d058687dc6eeeead55744651f
Naim <naim886@gmail.com>
2023-11-24 16:07:56 +0600

    design done

--- public/dashboard/js/script.js
+++ public/dashboard/js/script.js
@@ -1,10 +1,20 @@
 $('.waw-dropdown-container').on('click', function () {
-    $(this).children('.waw-dropdown').slideToggle()
-    $(this).children('a').children('.waw-menu-item-right-icon').toggleClass('fa-chevron-up fa-chevron-down')
-    $(this).siblings('.waw-dropdown-container').children('.waw-dropdown').slideUp()
-    $(this).siblings('.waw-dropdown-container').children('a').children('.waw-menu-item-right-icon').toggleClass('fa-chevron-down fa-chevron-up')
+    let display = $(this).children('.waw-dropdown').css('display')
+    if (display == 'none') {
+        $(this).children('.waw-dropdown').slideDown()
+        $(this).children('a').children('.waw-menu-item-right-icon').removeClass('fa-chevron-down').addClass('fa-chevron-up')
+        $(this).siblings('.waw-dropdown-container').children('.waw-dropdown').slideUp()
+        $(this).siblings('.waw-dropdown-container').children('a').children('.waw-menu-item-right-icon').removeClass('fa-chevron-up').addClass('fa-chevron-down')
+    } else {
+        $(this).children('.waw-dropdown').slideUp()
+        $(this).children('a').children('.waw-menu-item-right-icon').removeClass('fa-chevron-up').addClass('fa-chevron-down')
+    }
 })
 
-$('.waw-menu').children('.active').children('.waw-dropdown').slideToggle()
-$('.waw-menu').children('.active').children('a').children('.waw-menu-item-right-icon').toggleClass('fa-chevron-down fa-chevron-up')
+// $('.waw-menu').children('.active').children('.waw-dropdown').slideToggle()
+// $('.waw-menu').children('.active').children('a').children('.waw-menu-item-right-icon').toggleClass('fa-chevron-down fa-chevron-up')
+//
 
+$('.profile-section').on('click', function () {
+    $('.profile-dropdown').slideToggle()
+})
